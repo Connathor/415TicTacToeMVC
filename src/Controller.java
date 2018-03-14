9df477c894343a901c2135858ce0f6c44d1dbaa9
Jonathan Chambers <jonathan.chambers@selu.edu>
2018-03-13 19:10:11 -0500

    changed a bunch of shit

--- src/Controller.java
+++ src/Controller.java
@@ -1,14 +1,18 @@
-import java.awt.font.*;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class Controller implements ActionListener{
+    private Model model;
+	private View view;
+	
+	
+	public Controller(Model model, View view) {
+		this.model = model;
+		this.view = view;
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
 
-public class Controller {
-	
-	
-	
-	
-	
-	
 }
